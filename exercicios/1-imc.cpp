647d8c854239b9c8d8357c5bd00507aeda62e9d7
Yuri Magagnatto <yuri.magagnatto@gmail.com>
2023-09-15 00:18:21 -0300

    exercicio 1

--- exercicios/1-imc.cpp
+++ exercicios/1-imc.cpp
@@ -1,35 +1,31 @@
 #include <iostream>
-#include <string>
-#include <cmath>
 using namespace std;
 
 int main() {
     
     float imc, peso, altura;
-    
-    cout << "Bem-vindo ao SysHealth" << "\n\n";
+
     cout << "Qual seu peso em kg? ";
     cin >> peso;
     cout << "Qual sua altura em metros? ";
     cin >> altura;
     
     imc = peso / (altura * altura);
-    imc = round(imc * 100.0) / 100.0;
     cout << "Seu IMC é de: " << imc << "\n";
     
     if (imc < 18.5) {
-        cout << "Você está abaixo do peso";
+        cout << "Abaixo do peso";
     } else {
-        if (imc >= 18.5 && imc <= 24.9) {
+        if (imc >= 18.5 && imc < 25) {
             cout << "Peso normal";
         } else {
-            if (imc >= 25 && imc <= 29.9) {
-                cout << "Sobrepeso";
+            if (imc >= 25 && imc < 30) {
+                cout << "Acima do peso";
             } else {
-                if (imc >= 30 && imc <= 34.9) {
+                if (imc >= 30 && imc < 35) {
                     cout << "Obesidade I";
                 } else {
-                    if (imc >= 35 && imc <= 39.9) {
+                    if (imc >= 35 && imc < 40) {
                         cout << "Obesidade II";
                     } else {
                         cout << "Obesidade III";
@@ -38,6 +34,4 @@ int main() {
             }
         }
     }
-
-    return 0;
 }
